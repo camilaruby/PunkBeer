89fc506263c73cd8a732615e981c6e743cb6fd9f
Usuário Convidado <Guest@PA06MICRO21.local>
2017-11-27 18:03:54 -0200

    first commit

---
+++ PunkBeer/Beer.swift
@@ -0,0 +1,9 @@
+//
+//  Beer.swift
+//  PunkBeer
+//
+//  Created by Usuário Convidado on 27/11/17.
+//  Copyright © 2017 br.com.Fiap.camila. All rights reserved.
+//
+
+import Foundation
---
+++ PunkBeer/BeerCell.swift
@@ -0,0 +1,24 @@
+//
+//  BeerCell.swift
+//  PunkBeer
+//
+//  Created by Usuário Convidado on 22/11/17.
+//  Copyright © 2017 br.com.Fiap.camila. All rights reserved.
+//
+
+import UIKit
+
+class BeerCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
